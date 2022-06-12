a125024f0ae3b0343bbe6dee21e91f8309d39e11
namnlcDev <100658696+namnlcDev@users.noreply.github.com>
2022-06-12 23:07:21 +0700

    feat: implement search api

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,4 +58,126 @@
         },
     };
     imJs.m();
+
+    function getNFT() {
+        var keyword = $('.home .search-autocomplete .search-field input').val();
+        var action = $('.home .search-autocomplete').attr('action');
+        console.log('keywor', keyword);
+        console.log('action', action);
+        var elem = $('.home .content-search .list-item');
+        $('#loading').addClass('display');
+        $.ajax({
+            method: 'GET',
+            url: action + keyword,
+            contentType: 'application/json',
+            dataType: 'json',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            cache: false,
+            data: { limit: 10, page: 1 },
+            success: function (data, status) {
+                if (data.status === 1) {
+                    console.log('data', data)
+                    $('.home .welcome').addClass('d-none');
+                    var collection = data.data.collection;
+                    var token = data.data.token;
+                    if (collection.length > 0) {
+                        elem.empty();
+                        localStorage.setItem('token', JSON.stringify(token));
+                        localStorage.setItem('collection', JSON.stringify(collection));
+                        $.map(collection, function (item, index) {
+                            const { collection_name, logo_url, collection_address, items } = item;
+                            var replaceUrl = token.find(item => item.collection_address === collection_address)?.url;
+                            var randomUrl = 'https://source.unsplash.com/random/200x200?sig=1';
+                            console.log('replace', replaceUrl);
+                            var itemAppend = `
+                            <div class="item-nft">
+                                <div class="item-img hover">
+                                    <img
+                                    loading="lazy"
+                                    width="220"
+                                    height="180"
+                                    src=${logo_url || replaceUrl || randomUrl}
+                                    alt=${collection_address} />
+                                </div>
+                                <div class="item-title">
+                                <p class="title">${collection_name}</p>
+                                <p class="sub-title">${items} nft items</p>
+                                </div>
+                            </div>
+                            `
+                            if (index <= 7) {
+                                elem.append(itemAppend)
+                            }
+                            $('.home .recent-search').addClass('d-none');
+                        })
+                    } else {
+                        elem.empty();
+                        elem.append(`
+                            <div class="page-not-found">
+                                <div class="content"><img src="../assets/images/404.png" alt="404"/>
+                                <p>No collections found. Please try another keyword.</p>
+                                </div>
+                            </div>
+                        `)
+                    }
+                    console.log('status', status)
+
+                    console.log('item nft', localStorage.getItem('nft'))
+                }
+            },
+            error: function (e) {
+                console.log('e', e)
+            },
+            complete: function () {
+                $('#loading').removeClass('display')
+            }
+        })
+    }
+    $('#search-nft input').keydown(function (e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            return false;
+        }
+    })
+
+    $('#search-nft .icon-search').on('click', function (e) {
+        var keyword = $('#search-nft input').val();
+        if (keyword.length > 0) {
+            getNFT();
+        }
+    });
+
+    function getRecentSearch() {
+        var elem = $('.home .recent-search .list-item');
+        $('.home .welcome').addClass('d-none');
+        if (localStorage.getItem('collection')) {
+            $.map(JSON.parse(localStorage.getItem('collection')), function (item, index) {
+                const { collection_name, logo_url, collection_address, items } = item;
+                var replaceUrl = JSON.parse(localStorage.getItem('token')).find(item => item.collection_address === collection_address)?.url;
+                var randomUrl = 'https://source.unsplash.com/random/200x200?sig=1';
+                if (index <= 3) {
+                    elem.append(`
+                    <div class="item-nft">
+                        <div class="item-img hover">
+                            <img
+                            loading="lazy"
+                            width="220"
+                            height="180"
+                            src=${logo_url || replaceUrl || randomUrl}
+                            alt=${collection_address} />
+                        </div>
+                        <div class="item-title">
+                        <p class="title">${collection_name}</p>
+                        <p class="sub-title">${items} nft items</p>
+                        </div>
+                    </div>
+                    `)
+                }
+            })
+        }
+    }
+
+    getRecentSearch();
 })(jQuery, window);
